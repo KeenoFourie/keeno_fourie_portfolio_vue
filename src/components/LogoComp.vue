e736bbaee50d2453472a98a41095aa0491dfd3e1
KeenoFourie <keenofourie614@gmail.com>
2023-07-01 10:19:13 +0200

    logo done

---
+++ src/components/LogoComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img id="logo_styling" src="https://i.postimg.cc/Nf75cnvc/Etsy-Item-Listing-Photo.png" alt="Logo" loading="lazy"> 
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
