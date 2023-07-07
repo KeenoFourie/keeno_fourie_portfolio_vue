11bf260c3325265f9c00e846922ccd54fa11412f
KeenoFourie <keenofourie614@gmail.com>
2023-07-07 13:43:10 +0200

    update

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -6,22 +6,26 @@
             <form action="https://formspree.io/f/mnqkqnvz" method="POST">
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Name:</label>
-                  <input type="name" aria-describedby="nameHelp" placeholder="Please Insert Name..." style="color: white" required name="name">
+                  <input type="name" aria-describedby="nameHelp" placeholder="Please Insert Name..." style="color: white" required name="name" oninvalid="this.setCustomValidity('Please Enter Your Name')"
+            oninput="this.setCustomValidity('')">
                   <div id="nameHelp" class="form-text"></div>
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Surname:</label>
-                  <input type="surname" aria-describedby="surnameHelp" placeholder="Please Insert Surname..." style="color: white" required name="surname">
+                  <input type="surname" aria-describedby="surnameHelp" placeholder="Please Insert Surname..." style="color: white" required name="surname" oninvalid="this.setCustomValidity('Please Enter Your Surname')"
+            oninput="this.setCustomValidity('')">
                   <div id="surnameHelp" class="form-text"></div>
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email-address:</label>
-                  <input type="email" aria-describedby="emailHelp" placeholder="Please Insert Email-address..." style="color: white" required name="email">
+                  <input type="email" aria-describedby="emailHelp" placeholder="Please Insert Email-address..." style="color: white" required name="email" oninvalid="this.setCustomValidity('Please Enter Your Email-address')"
+            oninput="this.setCustomValidity('')">
                   <div id="emailHelp" class="form-text"></div>
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Comments:</label>
-                  <input type="comments" placeholder="Please Insert Your comments..." style="color: white" required name="comments">
+                  <input type="comments" placeholder="Please Insert Your comments..." style="color: white" required name="comments" oninvalid="this.setCustomValidity('Please Enter Comments')"
+            oninput="this.setCustomValidity('')">
                 </div>
                 <button type="submit" class="btn btn-transparent" id="submit_button">Submit</button>
               </form>
